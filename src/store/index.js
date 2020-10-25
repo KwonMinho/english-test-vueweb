395eda81c39218ed68e7f9e1842f466eb2de7f4f
minho <alsgh458@gmail.com>
2020-10-25 16:47:02 +0900

    add partial load, voice func

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,14 @@ import Vuex from 'vuex';
 import Vue from 'vue';
 import db from '../firebase/firebase-init'
 
+const GET_SIZE = 10;
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
     state: {
+        loadCnt: 0,
+        fullSize: 0,
         sentences: [],
         koreanTest: [],
         englishTest: [],
@@ -42,9 +45,36 @@ const store = new Vuex.Store({
                     updateRepeatCnt(v.index);
                 }
             })
+        },
+        loadMoreSentences(state, v) {
+            loadMore(v.type.toLowerCase());
+        },
+        addSentence(state, v) {
+            add(v.sentence, v.meaning)
         }
     }
 });
+
+
+async function add(sentence, meaning) {
+    await db.collection('english')
+        .doc(sentence)
+        .set({
+            'index': store.state.fullSize,
+            'meaning': meaning,
+            'repeatCnt': 0
+        });
+    ++store.state.fullSize;
+
+    const metaRef = await db.collection('english-meta').doc('meta');
+    metaRef.update({
+        size: store.state.fullSize
+    }).then(function () {
+        console.log("Successfully updated meta full size!");
+    }).catch(function (error) {
+        console.error("Error updating document: ", error);
+    });
+}
 
 async function updateRepeatCnt(index) {
     const sentence = store.state.sentences[index].sentence;
@@ -60,9 +90,13 @@ async function updateRepeatCnt(index) {
     });
 }
 
-async function loadSentences() {
-    const datas = await db.collection('english').get();
-    let index = 0;
+async function loadMore(type) {
+    const datas = await db.collection('english')
+        .limit(GET_SIZE)
+        .where("index", ">", store.state.loadCnt)
+        .orderBy("index", "asc")
+        .get();
+    let index = store.state.koreanTest.length;
     datas.forEach((data) => {
         store.state.sentences.push({
             'sentence': data.id,
@@ -73,9 +107,31 @@ async function loadSentences() {
         store.state.englishTest[index] = { 'index': index };
         ++index;
     })
-    store.state.isLoading = false;
-    store.state.curTest = store.state.koreanTest;
+    store.state.curTest = store.state[`${type}Test`];
 }
 
-loadSentences();
+async function initLoad() {
+    const datas = await db.collection('english').limit(GET_SIZE).get();
+    let index = store.state.koreanTest.length;
+    datas.forEach((data) => {
+        store.state.sentences.push({
+            'sentence': data.id,
+            'meaning': data.data().meaning,
+            'repeatCnt': data.data().repeatCnt,
+        });
+        store.state.koreanTest[index] = { 'index': index };
+        store.state.englishTest[index] = { 'index': index };
+        ++store.state.loadCnt;
+        ++index;
+    })
+    store.state.isLoading = false;
+    store.state.curTest = store.state.koreanTest;
+
+    const meta = await db.collection('english-meta').get();
+    meta.forEach((m) => {
+        store.state.fullSize = m.data().size;
+    })
+}
+
+initLoad();
 export default store;
